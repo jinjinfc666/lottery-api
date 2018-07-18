54c2b50856daa4b4052ecadda598d20507d68643
DESKTOP-J2BDIC2\Administrator <seayfhzl001@gmail.com>
2018-07-18 15:27:28 +0800

    解决不能编译

--- src/test/java/lottery/BigDecimalTest.java
+++ src/test/java/lottery/BigDecimalTest.java
@@ -11,8 +11,10 @@ public class BigDecimalTest {
 	public void test() {
 		BigDecimal val1 = new BigDecimal(1001);
 		BigDecimal val2 = new BigDecimal(100);
-		
-		Assert.isTrue(val1.divide(val2, 2, BigDecimal.ROUND_HALF_DOWN).floatValue() == 10.01);
+		BigDecimal val3 = new BigDecimal(10.01);
+		BigDecimal val4 = new BigDecimal(1);
+		int ret = val1.divide(val2, 2, BigDecimal.ROUND_HALF_DOWN).compareTo(val3.divide(val4, 2, BigDecimal.ROUND_HALF_DOWN));
+		Assert.isTrue(ret == 0);
 	}
 
 }
