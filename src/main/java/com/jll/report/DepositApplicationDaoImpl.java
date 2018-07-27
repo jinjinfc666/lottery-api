0231adc5a2743dad73b049b53e040d77df2e26ac
JV4ZA3XJM72U6JO\Administrator <Silence19920815@gmail.com>
2018-07-27 20:35:22 +0800

    充值管理和play_type表的增加和order_info表修改play_type的类型

--- src/main/java/com/jll/report/DepositApplicationDaoImpl.java
+++ src/main/java/com/jll/report/DepositApplicationDaoImpl.java
@@ -1,15 +1,20 @@
 package com.jll.report;
 
 
+import java.util.ArrayList;
+import java.util.Iterator;
+import java.util.List;
+
 import javax.persistence.NoResultException;
 
+import org.apache.commons.lang3.StringUtils;
+import org.hibernate.Session;
 import org.hibernate.SessionFactory;
 import org.hibernate.query.Query;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.orm.hibernate5.support.HibernateDaoSupport;
 import org.springframework.stereotype.Repository;
 
-import com.jll.entity.DepositApplication;
 
 
 
@@ -23,17 +28,47 @@ public class DepositApplicationDaoImpl extends HibernateDaoSupport implements De
 		super.setSessionFactory(sessionFactory);
 	}
 	@Override
-	public DepositApplication queryDetails(Integer id) {
-		String sql = "from DepositApplication where id=?";
-		DepositApplication dep = null;
-		Query<DepositApplication> query = currentSession().createQuery(sql, DepositApplication.class);
-		query.setParameter(0, id);
-		try {
-			dep = query.getSingleResult();
+	public List<?> queryDetails(String userName,String orderNum,String startTime,String endTime) {
+		List<Object> list=new ArrayList<Object>();
+		String userNameSql="";
+		String orderNumSql="";
+		String timeSql="";
+		if(!StringUtils.isBlank(userName)) {
+			userNameSql=" and b.userName=?";
+			list.add(userName);
+		}
+		if(!StringUtils.isBlank(orderNum)) {
+			orderNumSql=" and a.orderNum=?";
+			list.add(orderNum);
+		}
+		if(!StringUtils.isBlank(startTime)&&!StringUtils.isBlank(endTime)) {
+			timeSql=" and a.createTime >'"+startTime+"' and a.createTime <='"+endTime+"'";
+		}
+		String sql = "from DepositApplication a,UserInfo b,PayChannel c,PayType d where a.userId=b.id and a.payType=d.id and a.payChannel=c.id "+userNameSql+orderNumSql+timeSql+" order by a.id";
+		logger.debug(sql+"-----------------------------queryLoyTst----SQL--------------------------------");
+		Query<?> query = getSessionFactory().getCurrentSession().createQuery(sql);
+		Iterator<Object> it = list.iterator();
+		int a=0;
+        while(it.hasNext()){
+        	query.setParameter(a, it.next());
+        	a++;
+        }
+		List<?> list1 = new ArrayList<>();
+		try {			
+			list1 = query.list();
 		}catch(NoResultException ex) {
 			
 		}
-		return dep;
+		return list1;
+	}
+	@Override
+	public void updateState(Integer id, Integer state) {
+		Session session=getSessionFactory().getCurrentSession();
+		String hql = ("update DepositApplication set state=? where id=?");  
+		Query query = session.createQuery(hql);
+		query.setParameter(0, state);
+		query.setParameter(1, id);;
+		query.executeUpdate();
 	}
 	
 }
--- src/main/java/com/jll/report/DepositApplicationService.java
+++ src/main/java/com/jll/report/DepositApplicationService.java
@@ -1,7 +1,10 @@
 package com.jll.report;
 
-import com.jll.entity.DepositApplication;
+import java.util.List;
+import java.util.Map;
+
 
 public interface DepositApplicationService {
-	DepositApplication queryDetails(Integer id);
+	List<?> queryDetails(Map<String,Object> ret);
+	public void updateState(Map<String,Object> ret);
 }
