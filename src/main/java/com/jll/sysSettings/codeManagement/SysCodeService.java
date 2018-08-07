97f89b4aebb8d5ef798b8f1238b8e7e10e8db0dd
JV4ZA3XJM72U6JO\Administrator <Silence19920815@gmail.com>
2018-08-07 19:47:49 +0800

    no message

--- src/main/java/com/jll/sysSettings/codeManagement/SysCodeService.java
+++ src/main/java/com/jll/sysSettings/codeManagement/SysCodeService.java
@@ -6,21 +6,19 @@ import java.util.Map;
 import com.jll.entity.SysCode;
 
 public interface SysCodeService {
-	void saveBigSysCode(Map<String,Object> ret);
-	void saveSmallSysCode(Map<String,Object> ret);
+//	void saveBigSysCode(Map<String,Object> ret);
+//	void saveSmallSysCode(Map<String,Object> ret);
 	List<SysCode> quertBigType();
 	List<SysCode> querySmallType(Integer id);
 	void updateSyscode(Map<String,Object> ret);
 	List<SysCode> queryType(String bigType);
 	void updateBigState(Integer id,Integer state);
 	void updateSmallState(Integer id,Integer state);
-	List<SysCode> queryBigCodeName(Integer id);
+	SysCode querySysCodeById(Integer sysCodeTypeId);
 	List<SysCode> querySmallCodeName(Integer id);
 	boolean isNull(String codeName);
 	List<SysCode> queryCacheType(String codeName);
 	List<SysCode> queryCacheTypeOnly(String codeName);
-	
-	SysCode querySysCodeById(Integer sysCodeTypeId);
 	
 	void saveSysCode(SysCode sysCode);
 }
