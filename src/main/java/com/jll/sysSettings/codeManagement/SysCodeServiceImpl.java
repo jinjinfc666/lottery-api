049209d1983817e3fa9920cb94dc909e0699ca62
JV4ZA3XJM72U6JO\Administrator <Silence19920815@gmail.com>
2018-08-08 16:58:05 +0800

    no message

--- src/main/java/com/jll/sysSettings/codeManagement/SysCodeServiceImpl.java
+++ src/main/java/com/jll/sysSettings/codeManagement/SysCodeServiceImpl.java
@@ -21,30 +21,6 @@ public class SysCodeServiceImpl implements SysCodeService {
 	SysCodeService sysCodeService;
 	@Resource
 	CacheRedisService cacheRedisService;
-//	@Override
-//	public void saveBigSysCode(Map<String, Object> ret) {
-//		String codeName=(String) ret.get("codeName");
-//		String codeVal=(String) ret.get("codeVal");
-//		String remark=(String) ret.get("remark");
-//		Integer codeType=null;
-//		Integer isCodeType=1;
-//		Integer seq=null;
-//		Integer state=Constants.SysCodeState.VALID_STATE.getCode();
-//		sysCodeDao.saveSysCode(codeType, isCodeType, codeName, codeVal, seq, state, remark);
-//	}
-
-//	@Override
-//	public void saveSmallSysCode(Map<String, Object> ret) {
-//		String typeCodeName=(String) ret.get("typeCodeName");
-//		String codeName=(String) ret.get("codeName");
-//		String codeVal=(String) ret.get("codeVal");
-//		String remark=(String) ret.get("remark");
-//		Integer codeType=sysCodeDao.quertSysCodeId(typeCodeName);
-//		Integer seq=sysCodeDao.quertSysCodeSeq(codeType)+1;
-//		Integer isCodeType=0;
-//		Integer state=Constants.SysCodeState.VALID_STATE.getCode();
-//		sysCodeDao.saveSysCode(codeType, isCodeType, codeName, codeVal, seq, state, remark);
-//	}
 
 	@Override
 	public List<SysCode> quertBigType() {
@@ -144,9 +120,12 @@ public class SysCodeServiceImpl implements SysCodeService {
 	}
 
 	@Override
-	public SysCode querySysCodeById(Integer sysCodeTypeId) {
-		// TODO Auto-generated method stub
-		return null;
+	public SysCode querySysCodeById(Integer id) {
+		List<SysCode> list=sysCodeDao.queryBigCodeName(id);
+		if(list==null||list.size()==0) {
+			return null;
+		}
+		return list.get(0);
 	}
 
 	@Override
