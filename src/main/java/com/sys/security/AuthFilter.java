a799efec8f554ffb5018b90851940ecb83208fd5
DESKTOP-J2BDIC2\Administrator <seayfhzl001@gmail.com>
2018-10-08 21:56:44 +0800

    1，增加登录失败和成功处理机制 2，增加验证码的验证代码

---
+++ src/main/java/com/sys/security/AuthFilter.java
@@ -0,0 +1,39 @@
+package com.sys.security;
+
+import java.io.IOException;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import org.apache.log4j.Logger;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.AuthenticationException;
+import org.springframework.security.oauth2.provider.client.ClientCredentialsTokenEndpointFilter;
+
+public class AuthFilter extends ClientCredentialsTokenEndpointFilter {
+
+	private Logger logger = Logger.getLogger(AuthFilter.class);
+	
+	@Override
+	public void afterPropertiesSet() {
+		
+	}
+	
+	
+	@Override
+	public Authentication attemptAuthentication(HttpServletRequest request, HttpServletResponse response)
+			throws AuthenticationException, IOException, ServletException {
+		//TODO valid the captcha
+		String recCaptcha = null;
+		String saveCaptcha = null;
+		
+		if(recCaptcha == null || saveCaptcha == null)
+            throw new CusAuthenticationException(this.messages.getMessage("LoginAuthentication.captchaInvalid"));  
+        if(!saveCaptcha.equalsIgnoreCase(recCaptcha)){  
+            throw new CusAuthenticationException(this.messages.getMessage("LoginAuthentication.captchaNotEquals"));  
+        }
+        
+		return super.attemptAuthentication(request, response);
+	}
+}
