ede425504cf6a2eaa452dc185ada7257a2c3a501
DESKTOP-J2BDIC2\Administrator <seayfhzl001@gmail.com>
2018-11-24 16:45:58 +0800

    1,修改充值方式分类名称

--- src/main/java/com/jll/common/constants/Constants.java
+++ src/main/java/com/jll/common/constants/Constants.java
@@ -554,7 +554,7 @@ public class Constants {
 		CT_PLAY_TYPE_CLASSICFICATION("ct_play_type_classicfication"),//"玩法类型"
 		WITHDRAWAL_CFG("withdrawal_cfg"),
 		DEMO_USER_CFG("demo_user_cfg"),//试玩用户属性
-		PAY_TYPE("pay_type"),//充值方式
+		PAY_TYPE("pay_type_class"),//充值方式
 		SYS_RUNTIME_ARGUMENT("sys_runtime_argument");
 		private String value;
 		
